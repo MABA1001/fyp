600266ca65687a4be92e628ca21c7f198027f3b5
MABA1001 <mughalabdullah2905@gmail.com>
2023-04-30 21:48:19 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const stripe= require('stripe')('sk_test_51MmgZ7SGYRgpMBwBaCEGZPwwxWobAJvb4kfmvbmi31ekN7P5oSJGzPz2nF7wpE0nsYlzOLaWHdaPYPX7gAWCKxtS00kOhmlZfL')
+const cors= require('cors')
+const express = require('express')
+const bodyParser = require ('body-parser') 
+const app = express()
+app.use(cors());
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World naa!')
+})
+
+app.post('/payment-sheet', async (req, res) => {
+    // Use an existing Customer ID if this is a returning customer.
+    
+    const customer = await stripe.customers.create();
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      {customer: customer.id},
+      {apiVersion: '2022-11-15'}
+
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 6800,
+      currency: 'INR',
+      customer: customer.id,
+      payment_method_types : ['card'],
+    });
+  
+    res.json({
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id, 
+    });
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
